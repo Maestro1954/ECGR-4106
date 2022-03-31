7305cf9864e370b510aae8b74144d0fa21ed967a
Maestro1954 <83888575+Maestro1954@users.noreply.github.com>
2022-03-30 23:08:59 -0400

    Create acampos_hw3_p1.py

---
+++ Homework 3/acampos_hw3_p1.py
@@ -0,0 +1,231 @@
+# 1. Load and normalize CIFAR10
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+
+data_path = '../data-unversioned/p1ch7/'
+cifar10 = datasets.CIFAR10(data_path, train=True, download=True)
+cifar10_val = datasets.CIFAR10(data_path, train=False, download=True)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+class_names = ['plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+################################
+
+img, label = cifar10[99]
+img, label, class_names[label]
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+class Block(nn.Module):
+    def __init__(self, num_layers, in_channels, out_channels, identity_downsample=None, stride=1):
+        assert num_layers in [18, 34, 50, 101, 152], "should be a a valid architecture"
+        super(Block, self).__init__()
+        self.num_layers = num_layers
+        if self.num_layers > 34:
+            self.expansion = 4
+        else:
+            self.expansion = 1
+        # ResNet50, 101, and 152 include additional layer of 1x1 kernels
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        if self.num_layers > 34:
+            self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        else:
+            # for ResNet18 and 34, connect input directly to (3x3) kernel (skip first (1x1))
+            self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU()
+        self.identity_downsample = identity_downsample
+
+    def forward(self, x):
+        identity = x
+        if self.num_layers > 34:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+
+# Writing the network as a submodule:
+class ResNet(nn.Module):
+    def __init__(self, num_layers, block, image_channels, num_classes):
+        assert num_layers in [18, 34, 50, 101, 152], f'ResNet{num_layers}: Unknown architecture! Number of layers has ' \
+                                                     f'to be 18, 34, 50, 101, or 152 '
+        super(ResNet, self).__init__()
+        if num_layers < 50:
+            self.expansion = 1
+        else:
+            self.expansion = 4
+        if num_layers == 18:
+            layers = [2, 2, 2, 2]
+        elif num_layers == 34 or num_layers == 50:
+            layers = [3, 4, 6, 3]
+        elif num_layers == 101:
+            layers = [3, 4, 23, 3]
+        else:
+            layers = [3, 8, 36, 3]
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # ResNetLayers
+        self.layer1 = self.make_layers(num_layers, block, layers[0], intermediate_channels=64, stride=1)
+        self.layer2 = self.make_layers(num_layers, block, layers[1], intermediate_channels=128, stride=2)
+        self.layer3 = self.make_layers(num_layers, block, layers[2], intermediate_channels=256, stride=2)
+        self.layer4 = self.make_layers(num_layers, block, layers[3], intermediate_channels=512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * self.expansion, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+    def make_layers(self, num_layers, block, num_residual_blocks, intermediate_channels, stride):
+        layers = []
+
+        identity_downsample = nn.Sequential(nn.Conv2d(self.in_channels, intermediate_channels*self.expansion, kernel_size=1, stride=stride),
+                                            nn.BatchNorm2d(intermediate_channels*self.expansion))
+        layers.append(block(num_layers, self.in_channels, intermediate_channels, identity_downsample, stride))
+        self.in_channels = intermediate_channels * self.expansion # 256
+        for i in range(num_residual_blocks - 1):
+            layers.append(block(num_layers, self.in_channels, intermediate_channels)) # 256 -> 64, 64*4 (256) again
+        return nn.Sequential(*layers)
+
+model = ResNet()
+
+numel_list = [p.numel() for p in model.parameters()]
+sum(numel_list), numel_list
+
+##########################
+def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
+    for epoch in range(1, n_epochs + 1):
+        loss_train = 0.0
+        for imgs, labels in train_loader:
+            outputs = model(imgs)
+            loss = loss_fn(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+        
+        if epoch == 1 or epoch % 10 == 0:
+            print('{} Epoch {}, Training loss {}'.format(
+            datetime.datetime.now(), epoch,
+            loss_train / len(train_loader)))
+
+# In[31]:
+train_loader = torch.utils.data.DataLoader(cifar10, batch_size=64, shuffle=True)
+
+model = Net() #
+optimizer = optim.SGD(model.parameters(), lr=1e-2) #
+loss_fn = nn.CrossEntropyLoss() #
+
+training_loop(
+    n_epochs = 100,
+    optimizer = optimizer,
+    model = model,
+    loss_fn = loss_fn,
+    train_loader = train_loader,
+)
+
+# In[32]:
+train_loader = torch.utils.data.DataLoader(cifar10, batch_size=64, shuffle=False)
+
+val_loader = torch.utils.data.DataLoader(cifar10_val, batch_size=64, shuffle=False)
+
+def validate(model, train_loader, val_loader):
+    for name, loader in [("train", train_loader), ("val", val_loader)]:
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for imgs, labels in loader:
+                outputs = model(imgs)
+                _, predicted = torch.max(outputs, dim=1)
+                total += labels.shape[0]
+                correct += int((predicted == labels).sum())
+        
+        print("Accuracy {}: {:.2f}".format(name , correct / total))
+
+validate(model, train_loader, val_loader)
+
+# Save model to a file
+torch.save(model.state_dict(), data_path + 'birds_vs_airplanes.pt')
+
+# In[34]:
+loaded_model = Net()
+loaded_model.load_state_dict(torch.load(data_path + 'birds_vs_airplanes.pt'))
+
+# In[35]:
+device = (torch.device('cuda') if torch.cuda.is_available()
+else torch.device('cpu'))
+print(f"Training on device {device}.")
